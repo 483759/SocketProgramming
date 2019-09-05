db78166c6bc0117ae568b94b85df3834c42313b4
IJin Yun <483759@naver.com>
2019-09-05 15:17:37 +0900

    Update socket_client.c

--- socket_client.c
+++ socket_client.c
@@ -18,6 +18,7 @@ int main(){
 	int check;
 
 	client_socket = socket(PF_INET, SOCK_STREAM, 0);
+	//클라이언트 소켓 생성
 	
 	if(client_socket==-1){
 		printf("Client Socket ERROR");
@@ -28,8 +29,12 @@ int main(){
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(CLIENT_PORT);
 	server_address.sin_addr.s_addr = inet_addr(CLIENT_IP);
+	//family -> 소켓 생성시 지정한 프로토콜에 맞춰줌
+	//port -> 클라이언트 포트번호
+	//addr -> IP주소 저장
 
 	if(connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address))==-1){
+	//서버와 연결 설정을 위한 connect 호출
 		printf("Connect ERROR");
 		exit(0);
 	}
@@ -38,7 +43,7 @@ int main(){
 
 	while(1){
 		check=read(client_socket, Buff, sizeof(Buff));
-		if(client_socket==-1){
+		if(client_socket==-1){	//서버쪽에서 연결이 중지 될 때
 			printf("Disconnection Check\n");
 			close(client_socket);
 			break;
